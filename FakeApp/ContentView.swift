dd1cdead75e02278a7f098de9b74d0902bdf0015
DonHala <halapovaev@yandex.ru>
2024-06-01 13:03:16 +0300

    Вьюшка по типу таблицы

--- FakeApp/ContentView.swift
+++ FakeApp/ContentView.swift
@@ -34,7 +34,6 @@ struct CarExpenseRow: View {
                         .frame(width: 40)
                 }
                 Button(action: {
-                    // Действие для стрелки
                 }) {
                     Image(systemName: "chevron.down")
                         .foregroundColor(.blue)
@@ -47,30 +46,24 @@ struct CarExpenseRow: View {
     }
 }
 
+struct CarExpense: Identifiable {
+    var id = UUID()
+    var car: String
+    var amount: String
+    var percentage: String
+    var carDescript: String
+}
 
 struct ContentView: View {
     @State private var amount: String = "465 ₽"
     @State private var description: String = "Общая сумма расходов за 31.05"
     
-    @State private var car1: String = "С 329 АХ 40"
-    @State private var amount1: String = "215 ₽"
-    @State private var percentage1: String = "46%"
-    @State private var carDescript1: String = "(стария)"
-    
-    @State private var car2: String = "С 480 АС 40"
-    @State private var amount2: String = "125 ₽"
-    @State private var percentage2: String = "27%"
-    @State private var carDescript2: String = "(прадо)"
-    
-    @State private var car3: String = "У 177 АА 797"
-    @State private var amount3: String = "125 ₽"
-    @State private var percentage3: String = "27%"
-    @State private var carDescript3: String = "(порше 718)"
-    
-    @State private var car4: String = "Все"
-    @State private var amount4: String = "465 ₽"
-    @State private var percentage4: String = "100%"
-    @State private var carDescript4: String = ""
+    @State private var carExpenses = [
+        CarExpense(car: "С 329 АХ 40", amount: "215 ₽", percentage: "46%", carDescript: "(стария)"),
+        CarExpense(car: "С 480 АС 40", amount: "125 ₽", percentage: "27%", carDescript: "(прадо)"),
+        CarExpense(car: "У 177 АА 797", amount: "125 ₽", percentage: "27%", carDescript: "(порше 718)"),
+        CarExpense(car: "Все", amount: "465 ₽", percentage: "100%", carDescript: ""),
+    ]
     
     var body: some View {
         ScrollView {
@@ -84,13 +77,10 @@ struct ContentView: View {
                         .font(.caption)
                         .padding()
                     Divider()
-                    CarExpenseRow(car: $car1, amount: $amount1, percentage: $percentage1, carDescript: $carDescript1)
-                    Divider()
-                    CarExpenseRow(car: $car2, amount: $amount2, percentage: $percentage2, carDescript: $carDescript2)
-                    Divider()
-                    CarExpenseRow(car: $car3, amount: $amount3, percentage: $percentage3, carDescript: $carDescript3)
-                    Divider()
-                    CarExpenseRow(car: $car4, amount: $amount4, percentage: $percentage4, carDescript: $carDescript4)
+                    ForEach($carExpenses) { $expense in
+                        CarExpenseRow(car: $expense.car, amount: $expense.amount, percentage: $expense.percentage, carDescript: $expense.carDescript)
+                        Divider()
+                    }
                 }
                 .background(Color(.systemGray6))
             }
