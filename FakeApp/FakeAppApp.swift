56246d91ac4f50028194ccfdcfb1308ee835107d
DonHala <halapovaev@yandex.ru>
2024-06-01 07:31:06 +0300

    first commit

---
+++ FakeApp/FakeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeAppApp.swift
+//  FakeApp
+//
+//  Created by DonHalab on 01.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FakeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
