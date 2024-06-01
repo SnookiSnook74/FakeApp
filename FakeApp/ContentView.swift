7575c44d842b948144a6c25eb0ed4684057f7978
DonHala <halapovaev@yandex.ru>
2024-06-01 07:50:13 +0300

    Календарь (снизу)

--- FakeApp/ContentView.swift
+++ FakeApp/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+struct DateView: View {
+    let dates = [
+        ("24.05", "ПТ"),
+        ("25.05", "СБ"),
+        ("26.05", "ВС"),
+        ("27.05", "ПН"),
+        ("28.05", "ВТ"),
+        ("29.05", "СР"),
+        ("30.05", "ЧТ"),
+        ("31.05", "ПТ")
+    ]
+    
+    var body: some View {
+        LazyHStack(spacing: 20) {
+            ForEach(dates, id: \.0) { date in
+                VStack {
+                    Text(date.0)
+                        .font(.system(size: 10))
+                        .foregroundColor(.gray)
+                    Text(date.1)
+                        .font(.system(size: 10))
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            DateView()
+            Spacer()
         }
-        .padding()
     }
 }
 
